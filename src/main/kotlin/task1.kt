39829680c2ac1b333cd19a74c2cbe53ed07fc2ed
Сергей Кудашкин <ya.sikuda@yandex.ru>
2022-11-04 18:35:29 +0300

    add Long to Fraction

--- src/main/kotlin/task1.kt
+++ src/main/kotlin/task1.kt
@@ -1,50 +1,54 @@
-import java.lang.Integer.min
+import java.lang.Long.min
 
 fun main(){
 
-    val fraction1 = Fraction(1024,1022)
-    val fraction2 = Fraction(1023,1021)
+    val fraction1 = Fraction((Int.MAX_VALUE-3),1024)
+    val fraction2 = Fraction(Int.MAX_VALUE-1,1000)
 
-    val sum = fraction1 + fraction2
-    println(sum)
+    val sum1 = fraction1 + fraction2
+    println(sum1)
 
+    val fraction3 = Fraction((Long.MAX_VALUE-13),1024L)
+    val fraction4 = Fraction(Long.MAX_VALUE-1000,1000L)
+
+    val sum2 = fraction3 + fraction4
+    println(sum2)
 }
 
-
-class Fraction( var numerator:Int, var denumerator: Int ){
+class Fraction( var numerator: Long, var denumerator: Long ){
 
     init{
-        if (denumerator == 0)  throw ArithmeticException("Divide by zero")
+        if (denumerator == 0L)  throw ArithmeticException("Divide by zero")
         normalize()
     }
-
-    private val minpart get() = min(numerator,denumerator)
+    constructor(numerator:Int, denumerator:Int) : this(numerator.toLong(), denumerator.toLong())
+    private val minOf get() = min(numerator,denumerator)
 
     operator fun plus(other: Fraction): Fraction {
         val numerator = this.numerator*other.denumerator + this.denumerator*other.numerator
-        val denumerator = this.denumerator*other.denumerator
-        return Fraction(numerator, denumerator)
+        val enumerator = this.denumerator*other.denumerator
+        return Fraction(numerator, enumerator)
     }
 
     operator fun minus(other: Fraction): Fraction {
-        val numerator = this.numerator*other.denumerator - this.denumerator*other.numerator
+        val numerator = this.numerator*other.denumerator -  this.denumerator*other.numerator
         val denumerator = this.denumerator*other.denumerator
         return Fraction(numerator, denumerator)
     }
 
     private fun normalize(){
-       var i = minpart
+       var i = minOf
        while( i > 1 ) {
-           if(numerator%i == 0 && denumerator%i == 0 ){
+           if( numerator.mod(i) == 0L && denumerator.mod(i) == 0L ){
                numerator /= i
                denumerator /= i
-               i = minpart
+               i = minOf
            } else i--
        }
     }
 
     override fun toString(): String {
-        return "Fraction($numerator/$denumerator)"
+        return "Fraction($numerator/$denumerator)=${numerator/denumerator.toDouble()}"
     }
 
     override fun equals(other: Any?): Boolean {
@@ -60,7 +64,7 @@ class Fraction( var numerator:Int, var denumerator: Int ){
     }
 
     override fun hashCode(): Int {
-       return 100000 * numerator / denumerator
+       return (100000 * numerator / denumerator).toInt()
     }
 
 }
