d60e1a43ac01342c80e62802d9caede6a10edb17
Sergey Kudashkin <ya.sikuda@yandex.ru>
2022-11-16 15:04:56 +0300

    find error

--- src/main/kotlin/homework8/IdCard.kt
+++ src/main/kotlin/homework8/IdCard.kt
@@ -1,5 +1,5 @@
 package homework8
 
-import java.util.Date
+import java.util.*
 
-class IdCard(val number: Int = 0, dateExpire: Date = Date())
+class IdCard(val number: Int = 0, val dateExpire: Date = Date())
