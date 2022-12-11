6ffdd92774d7f3eafae2bd71a00202f9a0d11209
Сергей Кудашкин <ya.sikuda@yandex.ru>
2022-12-12 00:09:23 +0300

    task 1

--- src/main/kotlin/task1.kt
+++ src/main/kotlin/task1.kt
@@ -1,3 +1,44 @@
- fun main(){
+import java.io.File
 
- }
+fun checkPermission(item: Pair<String, List<Modificator>>): List<Pair<Modificator, Boolean>> {
+    val file = File(item.first)
+    val result = mutableListOf<Pair<Modificator, Boolean>>()
+    item.second.forEach() {
+        when (it) {
+            Modificator.READ ->
+                if(file.canRead()) result.add(Modificator.READ to true)
+                else result.add(Modificator.READ to false)
+            Modificator.WRITE ->
+                if(file.canWrite()) result.add(Modificator.WRITE to true)
+            else result.add(Modificator.WRITE to false)
+            Modificator.EXECUTE ->
+                if(file.canExecute()) result.add(Modificator.EXECUTE to true)
+                else result.add(Modificator.EXECUTE to false)
+        }
+    }
+    return result.toList()
+}
+
+enum class Modificator {
+    READ,
+    WRITE,
+    EXECUTE
+}
+
+fun main() {
+
+    val list = listOf(
+        "test1.txt" to listOf(Modificator.READ),
+        "test2.txt" to listOf(Modificator.READ, Modificator.WRITE),
+        "test3.txt" to listOf(Modificator.EXECUTE),
+    )
+
+    list.forEach { it ->
+        val permission = checkPermission(it)
+        println(it.first)
+        permission.forEach { elem ->
+            println("${elem.first} -> ${elem.second}")
+        }
+    }
+}
+
--- src/test/kotlin/Task1Test.kt
+++ src/test/kotlin/Task1Test.kt
@@ -1,9 +1,19 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class Task1Test {
 
     @Test
     fun test(){
+        val list = listOf(
+            "test1.txt" to listOf(Modificator.READ),
+            "test2.txt" to listOf(Modificator.READ, Modificator.WRITE),
+            "test3.txt" to listOf(Modificator.EXECUTE),
+        )
+
+        assertEquals(checkPermission(list[0]), listOf(Modificator.READ to true), "Первый элемент не совпал")
+        assertEquals(checkPermission(list[1]), listOf(Modificator.READ to true, Modificator.WRITE to true), "Второй элемент не совпал")
+        assertEquals(checkPermission(list[2]), listOf(Modificator.EXECUTE to false), "Третий элемент не совпал")
 
     }
 }
