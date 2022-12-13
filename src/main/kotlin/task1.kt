f42e6011a3656ef0e04ed679800ca1a011d91b88
Sergey Kudashkin <ya.sikuda@yandex.ru>
2022-12-13 10:48:39 +0300

    task 1 complite

--- src/main/kotlin/task1.kt
+++ src/main/kotlin/task1.kt
@@ -1,44 +1,41 @@
 import java.io.File
 
-fun checkPermission(item: Pair<String, List<Modificator>>): List<Pair<Modificator, Boolean>> {
-    val file = File(item.first)
-    val result = mutableListOf<Pair<Modificator, Boolean>>()
-    item.second.forEach() {
-        when (it) {
-            Modificator.READ ->
-                if(file.canRead()) result.add(Modificator.READ to true)
-                else result.add(Modificator.READ to false)
-            Modificator.WRITE ->
-                if(file.canWrite()) result.add(Modificator.WRITE to true)
-            else result.add(Modificator.WRITE to false)
-            Modificator.EXECUTE ->
-                if(file.canExecute()) result.add(Modificator.EXECUTE to true)
-                else result.add(Modificator.EXECUTE to false)
-        }
-    }
-    return result.toList()
+fun checkPermission(item: Pair<String, List<String>>, operation: String): Boolean {
+
+    return item.second.contains(operation)
+
 }
 
-enum class Modificator {
-    READ,
-    WRITE,
-    EXECUTE
+fun fillListFiles(filemame: String): List<Pair<String, List<String>>> {
+    val file = File(filemame)
+    val listFiles: MutableList<Pair<String, List<String>>> = mutableListOf()
+
+    if (!file.canRead()) return listOf()
+    file.readLines()
+        .forEach {
+            val pairFile = it.split("-")
+
+            val modificator =
+                if (pairFile.size > 1) pairFile[1].trim().split(",")
+                else listOf<String>()
+
+            listFiles.add(Pair(pairFile[0].trim(), modificator))
+        }
+
+    return listFiles.toList()
 }
 
 fun main() {
 
-    val list = listOf(
-        "test1.txt" to listOf(Modificator.READ),
-        "test2.txt" to listOf(Modificator.READ, Modificator.WRITE),
-        "test3.txt" to listOf(Modificator.EXECUTE),
-    )
+    val listFiles = fillListFiles("files.txt")
 
-    list.forEach { it ->
-        val permission = checkPermission(it)
-        println(it.first)
-        permission.forEach { elem ->
-            println("${elem.first} -> ${elem.second}")
-        }
+    //check one operation on list
+    val checkOperation = "DELETE"
+
+    listFiles.forEach { it ->
+        val permit = checkPermission(it, checkOperation)
+        println("${it.first} $checkOperation -> $permit")
+
     }
 }
 
--- src/test/kotlin/Task1Test.kt
+++ src/test/kotlin/Task1Test.kt
@@ -4,16 +4,11 @@ import kotlin.test.assertEquals
 class Task1Test {
 
     @Test
-    fun test(){
-        val list = listOf(
-            "test1.txt" to listOf(Modificator.READ),
-            "test2.txt" to listOf(Modificator.READ, Modificator.WRITE),
-            "test3.txt" to listOf(Modificator.EXECUTE),
-        )
+    fun test() {
+        val list = fillListFiles("test1.txt")
 
-        assertEquals(checkPermission(list[0]), listOf(Modificator.READ to true), "Первый элемент не совпал")
-        assertEquals(checkPermission(list[1]), listOf(Modificator.READ to true, Modificator.WRITE to true), "Второй элемент не совпал")
-        assertEquals(checkPermission(list[2]), listOf(Modificator.EXECUTE to false), "Третий элемент не совпал")
+        assertEquals(checkPermission(list[0], "DELETE"), false, "Первый элемент не модификатора DELETE")
+        assertEquals(list[0].second.size, 0, "У первого элемента нет модификаторов")
 
     }
 }
